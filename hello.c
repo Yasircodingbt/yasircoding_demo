af0ed82587ccfdbbbdcc9d1d046b21fc9a0bfccd
yasircodingbt <yasircoding_bt@outlook.com>
2024-10-28 18:28:15 +0500

    print number any number

--- hello.c
+++ hello.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
 int main(){
-printf("helo word");
-printf(" hello yasir");
+     int i,j,n;
+     printf("enter a num :");
+     scanf("%d",&n);
+     for (int i = 1; i <=10; i++)
+     {
+       printf("%d *%i = %d \n",n,i,n*i);
+     }        
+     
 return 0;
 }
+  
